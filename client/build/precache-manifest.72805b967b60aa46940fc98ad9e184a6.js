52f10d9f32798639028649e94b9f47855096f4fb
Dzenis H <dzenis.hankusic@gmail.com>
2023-06-04 12:24:14 +0200

    Add build folder

---
+++ client/build/precache-manifest.72805b967b60aa46940fc98ad9e184a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a775fc5fbc4820e1d84678dcf04eaf9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2beb9527906996cf4c3",
+    "url": "/static/css/main.99937756.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "f2beb9527906996cf4c3",
+    "url": "/static/js/main.3baf0da0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a568dbe7d1e376ac005b0227e8bdcc1",
+    "url": "/static/media/showcase.6a568dbe.jpg"
+  }
+]);
