c97d3f86b1fb08f141dcdae096edfcf84b45c292
Dzenis H <dzenis.hankusic@gmail.com>
2023-06-04 12:19:33 +0200

    V2.0.2

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from "react";
+
+export default () => {
+  return (
+    <Fragment>
+      <footer>
+        <article id="wrap" className="text-center">
+          <p className="display-6 text-center text-info mt-5">
+            Dzenis H.
+            <span role="img" aria-label="dash" className="text-center">
+              ➖
+            </span>
+            Dev_Spot
+            <span role="img" aria-label="dash" className="text-center">
+              ➖
+            </span>
+            Copyright © {new Date().getFullYear()}
+          </p>
+          <article id="lightings">
+            <section id="one">
+              <section id="two">
+                <section id="three">
+                  <section id="four">
+                    <section id="five" />
+                  </section>
+                </section>
+              </section>
+            </section>
+          </article>
+        </article>
+      </footer>
+    </Fragment>
+  );
+};
---
+++ client/src/components/not-found/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import notFound from "../../img/404.png";
+
+export default () => {
+  return (
+    <Fragment>
+     
+      <img src={notFound} />
+    </Fragment>
+  );
+};
